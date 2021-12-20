31ebfccd63b51ef3bada8f4d07977faa2d5475a3
muttahirm <95257111+muttahirm@users.noreply.github.com>
2021-12-20 02:41:43 +0100

    extract the category in the catalog screen

--- lib/src/views/catalog/catalog_screen.dart
+++ lib/src/views/catalog/catalog_screen.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/material.dart';
-// import '../../widgets/widgets.dart';
+
+import 'package:shopping_cart/src/models/models.dart';
+import 'package:shopping_cart/src/widgets/widgets.dart';
 
 // ignore: use_key_in_widget_constructors
 class CatalogScreen extends StatelessWidget {
-
   static const String routeName = '/catalog';
 
-  static Route route() {
+  static Route route({required Category category}) {
     return MaterialPageRoute(
-      settings: const RouteSettings(name: routeName),
-      builder: (_) => CatalogScreen(),);
+        settings: const RouteSettings(name: routeName),
+        builder: (_) => CatalogScreen(category: category));
   }
+
+  final Category category;
+
+  const CatalogScreen({
+    Key? key,
+    required this.category,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: CustomAppBar(title: 'Zara to Shop'),
-      // bottomNavigationBar: const CustomerNavigationBar(),
+      appBar: CustomAppBar(title: category.name),
+      bottomNavigationBar: const CustomerNavigationBar(),
       body: Container(),
     );
   }
