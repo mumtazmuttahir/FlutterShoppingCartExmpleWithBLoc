0bfa05c60e2026a5bc7da19eaa84db2e3d16fe11
muttahirm <95257111+muttahirm@users.noreply.github.com>
2021-12-20 02:42:47 +0100

    add an import

--- lib/src/views/wishlist/wishlist_screen.dart
+++ lib/src/views/wishlist/wishlist_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../widgets/widgets.dart';
+// import '../../widgets/widgets.dart';
 
 // ignore: use_key_in_widget_constructors
 class WishlistScreen extends StatelessWidget {
