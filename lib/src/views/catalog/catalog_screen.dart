522933fc3427694dd1a6311a65be68375e980e48
muttahirm <95257111+muttahirm@users.noreply.github.com>
2021-12-20 04:24:48 +0100

    create catalog screen

--- lib/src/views/catalog/catalog_screen.dart
+++ lib/src/views/catalog/catalog_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:shopping_cart/src/models/models.dart';
 import 'package:shopping_cart/src/widgets/widgets.dart';
 
@@ -22,10 +21,29 @@ class CatalogScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    List<Product> categoryProducts = Product.products.where((product) => product.category == category.name).toList();
+
     return Scaffold(
       appBar: CustomAppBar(title: category.name),
       bottomNavigationBar: const CustomerNavigationBar(),
-      body: Container(),
+      body: GridView.builder(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16.0,
+          vertical: 16.0,
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.15,
+        ),
+        itemCount: categoryProducts.length,
+        itemBuilder: (context, index) {
+          return ProdcutCard(
+            product: categoryProducts[index],
+            widthFctor: 2.3,
+          );
+        },
+      ),
     );
   }
 }
