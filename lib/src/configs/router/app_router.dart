76b7e2cf649442acd3ffd7c371bbae3ae9ab125f
muttahirm <95257111+muttahirm@users.noreply.github.com>
2021-12-20 04:24:27 +0100

    pass the arguement to Product Screen

--- lib/src/configs/router/app_router.dart
+++ lib/src/configs/router/app_router.dart
@@ -20,7 +20,7 @@ class AppRouter {
       case CartScreen.routeName:
         return CartScreen.route();
       case ProductScreen.routeName:
-        return ProductScreen.route();
+        return ProductScreen.route(product: settings.arguments as Product);
       case WishlistScreen.routeName:
         return WishlistScreen.route();
       case CatalogScreen.routeName:
