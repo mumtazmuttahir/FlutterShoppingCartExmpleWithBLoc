824e51f3be7b34133353f067e8e5fb9202ff060a
muttahirm <95257111+muttahirm@users.noreply.github.com>
2021-12-20 04:24:59 +0100

    create product screen

--- lib/src/views/product/product_screen.dart
+++ lib/src/views/product/product_screen.dart
@@ -1,23 +1,181 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/src/models/models.dart';
+import 'package:shopping_cart/src/widgets/widgets.dart';
 // import '../../widgets/widgets.dart';
 
 // ignore: use_key_in_widget_constructors
 class ProductScreen extends StatelessWidget {
-
   static const String routeName = '/product';
 
-  static Route route() {
+  static Route route({required Product product}) {
     return MaterialPageRoute(
       settings: const RouteSettings(name: routeName),
-      builder: (_) => ProductScreen(),);
+      builder: (_) => ProductScreen(product),
+    );
   }
+
+  final Product product;
+
+  // ignore: use_key_in_widget_constructors
+  const ProductScreen(
+    this.product,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: CustomAppBar(title: 'Zara to Shop'),
-      // bottomNavigationBar: const CustomerNavigationBar(),
-      body: Container(),
+        appBar: CustomAppBar(title: product.name),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.black,
+          child: SizedBox(
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.share,
+                      color: Colors.white,
+                    )),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(primary: Colors.white),
+                  child: Text(
+                    'ADD TO CART',
+                    style: Theme.of(context).textTheme.headline3!,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: ProductCarousel(product: product));
+  }
+}
+
+class ProductCarousel extends StatelessWidget {
+  const ProductCarousel({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            aspectRatio: 1.5,
+            viewportFraction: 0.9,
+            enlargeStrategy: CenterPageEnlargeStrategy.height,
+            enlargeCenterPage: true,
+            enableInfiniteScroll: false,
+            initialPage: 2,
+            autoPlay: true,
+          ),
+          items: [
+            //----<Image Seciton Start>----//
+            HeroCarouselCard(
+              product: product,
+            ),
+            //----<Image Seciton End>----//
+          ],
+        ),
+        //----<Name and Price Seciton Start>----//
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Stack(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                color: Colors.black.withAlpha(50),
+                alignment: Alignment.bottomCenter,
+              ),
+              Container(
+                margin: const EdgeInsets.all(5.0),
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                color: Colors.black,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        product.name,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5!
+                            .copyWith(color: Colors.white),
+                      ),
+                      Text(
+                        '${product.price}',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6!
+                            .copyWith(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        //----<Name and Price Seciton End>----//
+        //----<Product Information Seciton Start>----//
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: ExpansionTile(
+            initiallyExpanded: true,
+            title: Text(
+              'Product Information',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            children: [
+              ListTile(
+                title: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipisici elit, … ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ),
+            ],
+          ),
+        ),
+        //----<Product Information Seciton Start>----//
+        //----<Delivery Information Seciton Start>----//
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: ExpansionTile(
+            initiallyExpanded: false,
+            title: Text(
+              'Product Information',
+              style: Theme.of(context).textTheme.headline3,
+            ),
+            children: [
+              ListTile(
+                title: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipisici elit, … ist ein Blindtext, der nichts bedeuten soll, sondern als Platzhalter im Layout verwendet wird, um einen Eindruck vom fertigen Dokument zu erhalten',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ),
+            ],
+          ),
+        ),
+        //----<Delivery Information Seciton Start>----//
+      ],
     );
   }
 }
