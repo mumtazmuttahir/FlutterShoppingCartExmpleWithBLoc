a8272b2fcce799c6b6383a07bc566a3c91939281
muttahirm <95257111+muttahirm@users.noreply.github.com>
2022-01-10 04:52:19 +0100

    create methods

---
+++ lib/src/models/cart/cart_model.dart
@@ -0,0 +1,86 @@
+import 'package:equatable/equatable.dart';
+
+import '../models.dart';
+
+class Cart extends Equatable {
+  const Cart();
+
+  double get subTotal => products.fold(0, (total, current) => total + current.price);
+  String get subTotalString => subTotal.toStringAsFixed(2);
+
+  double deliveryFee (subTotal) {
+    if (subTotal >= 30.0) {
+      return 0.0;
+    }
+    return 10.0;
+  }
+
+  String get deliveryFeeString => deliveryFee(subTotal).toStringAsFixed(2);
+
+  String freeDelivery(subTotal) {
+    if(subTotal >= 30.0) {
+      return 'You have a free Delivery';
+    }
+    double missing = 30.0 - subTotal;
+    return 'Add \$ ${missing.toStringAsFixed(2)} for FREE Delivery';
+  }
+
+  String get freeDeliveryString => freeDelivery(subTotal);
+
+  double total(subTotal, deliveryFee) {
+    return subTotal + deliveryFee(subTotal);
+  }
+
+  String get totalString => total(subTotal, deliveryFee).toStringAsFixed(2);
+
+  static List<Product> products = [
+    const Product(
+      name: 'Soft Drink #1',
+      category: "Soft Drink",
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: true,
+    ),
+    const Product(
+      name: 'Soft Drink #1',
+      category: "Soft Drink",
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: true,
+    ),
+    const Product(
+      name: 'Soft Drink #1',
+      category: "Soft Drink",
+      imageUrl:
+          'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: true,
+    ),
+    const Product(
+      name: 'Smoothies #1',
+      category: "Smoothies",
+      imageUrl:
+          'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+      price: 2.99,
+      isRecommended: true,
+      isPopular: true,
+    ),
+    const Product(
+      name: 'Smoothies #2',
+      category: "Smoothies",
+      imageUrl:
+          'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+      price: 2.99,
+      isRecommended: false,
+      isPopular: true,
+    ),
+  ];
+
+  @override
+  List<Object?> get props => [];
+}
