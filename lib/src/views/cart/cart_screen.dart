c760b6790ea38ae30325fa1f4fd3bbd41ad8ba7c
muttahirm <95257111+muttahirm@users.noreply.github.com>
2022-01-10 18:50:31 +0100

    add the cart bloc builder

--- lib/src/views/cart/cart_screen.dart
+++ lib/src/views/cart/cart_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopping_cart/src/blocs/cart/cart_bloc.dart';
 import 'package:shopping_cart/src/models/models.dart';
 import 'package:shopping_cart/src/widgets/widgets.dart';
 // import '../../widgets/widgets.dart';
@@ -39,149 +41,166 @@ class CartScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Text(
-                        const Cart().freeDeliveryString,
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/');
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.black,
-                          shape: const RoundedRectangleBorder(),
-                          elevation: 0,
-                        ),
-                        child: Text(
-                          'Add More Items',
-                          style:
-                              Theme.of(context).textTheme.headline5!.copyWith(
-                                    color: Colors.white,
-                                    fontSize: 12,
-                                  ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  height: 300,
-                  child: ListView.builder(
-                    itemCount: Cart.products.length,
-                    itemBuilder: (context, index) {
-                      return CartProductCard(
-                        product: Cart.products[index],
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                const Divider(thickness: 2),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 30,
-                    vertical: 10,
-                  ),
-                  child: Column(
+      body: BlocBuilder<CartBloc, CartState>(
+        builder: (context, state) {
+          if (state is CartLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+          }
+          if (state is CartLoaded) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
                     children: [
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            'SUBTOTAL',
-                            style: Theme.of(context).textTheme.headline5,
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              const Cart().freeDeliveryString,
+                              style: Theme.of(context).textTheme.headline5,
+                            ),
                           ),
-                          Text(
-                            '\$ ${const Cart().subTotalString}',
-                            style: Theme.of(context).textTheme.headline5,
+                          Expanded(
+                            flex: 2,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/');
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.black,
+                                shape: const RoundedRectangleBorder(),
+                                elevation: 0,
+                              ),
+                              child: Text(
+                                'Add More Items',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline5!
+                                    .copyWith(
+                                      color: Colors.white,
+                                      fontSize: 12,
+                                    ),
+                              ),
+                            ),
                           ),
                         ],
                       ),
                       const SizedBox(
                         height: 10,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      SizedBox(
+                        height: 300,
+                        child: ListView.builder(
+                          itemCount: state.cart.products.length,
+                          itemBuilder: (context, index) {
+                            return CartProductCard(
+                              product: state.cart.products[index],
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Divider(thickness: 2),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 10,
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'SUBTOTAL',
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                                Text(
+                                  '\$ ${const Cart().subTotalString}',
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'DELIVERY FEE',
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                                Text(
+                                  '\$ ${const Cart().deliveryFeeString}',
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Stack(
                         children: [
-                          Text(
-                            'DELIVERY FEE',
-                            style: Theme.of(context).textTheme.headline5,
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: 60.0,
+                            decoration: BoxDecoration(
+                              color: Colors.black.withAlpha(50),
+                            ),
                           ),
-                          Text(
-                            '\$ ${const Cart().deliveryFeeString}',
-                            style: Theme.of(context).textTheme.headline5,
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: const EdgeInsets.all(5.0),
+                            height: 50.0,
+                            decoration: const BoxDecoration(
+                              color: Colors.black,
+                            ),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    'TOTAL',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline5!
+                                        .copyWith(color: Colors.white),
+                                  ),
+                                  Text(
+                                    '\$ ${const Cart().totalString}',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline5!
+                                        .copyWith(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
                         ],
                       ),
                     ],
                   ),
-                ),
-                Stack(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 60.0,
-                      decoration: BoxDecoration(
-                        color: Colors.black.withAlpha(50),
-                      ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: const EdgeInsets.all(5.0),
-                      height: 50.0,
-                      decoration: const BoxDecoration(
-                        color: Colors.black,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'TOTAL',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline5!
-                                  .copyWith(color: Colors.white),
-                            ),
-                            Text(
-                              '\$ ${const Cart().totalString}',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline5!
-                                  .copyWith(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
+                ],
+              ),
+            );
+          }
+          return const Text ('Something went wrong');
+        },
       ),
     );
   }
