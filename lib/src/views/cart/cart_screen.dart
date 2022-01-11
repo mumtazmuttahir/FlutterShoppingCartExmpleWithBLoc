c2f9ec532e14a4d6fe27522a92e15c0afb008341
mumtazmutthir <muttahir.mumtaz@hotmail.com>
2022-01-11 01:30:53 +0100

    show the products as per the map

--- lib/src/views/cart/cart_screen.dart
+++ lib/src/views/cart/cart_screen.dart
@@ -97,10 +97,11 @@ class CartScreen extends StatelessWidget {
                       SizedBox(
                         height: 300,
                         child: ListView.builder(
-                          itemCount: state.cart.products.length,
+                          itemCount: state.cart.productQuantity(state.cart.products).length,
                           itemBuilder: (context, index) {
                             return CartProductCard(
-                              product: state.cart.products[index],
+                              product: state.cart.productQuantity(state.cart.products).keys.elementAt(index),
+                              quantity: state.cart.productQuantity(state.cart.products).values.elementAt(index),
                             );
                           },
                         ),
